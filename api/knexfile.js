4c71bd594cc3e73cd0feee311423ca2c69a96261
Thomas <tmalidin33@gmail.com>
2021-04-05 16:41:05 +0200

    knex file production

--- api/knexfile.js
+++ api/knexfile.js
@@ -19,5 +19,16 @@ module.exports = {
             },
         },
     },
-    production: {},
+    production: {
+        client: 'pg',
+        connection: process.env.DATABASE_URL,
+        pool: {
+            min: 2,
+            max: 10,
+        },
+        migrations: {
+            tablename: 'knex_migrations',
+            directory: './data/migrations',
+        },
+    },
 };
